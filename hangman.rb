d0209e41c7f7752b2732e008a68bc19121bfc5e0
Brent Vatne <brentvatne@gmail.com>
2011-08-08 20:28:45 -0700

    diff and guess basic functionality done

--- hangman.rb
+++ hangman.rb
@@ -1,19 +1,43 @@
 class Hangman
-  attr_accessor :puzzle, :solution
+  attr_accessor :puzzle, :solution, :solution_diff
 
   def load(puzzle_filepath, solution_filepath)
     @puzzle = File.open(puzzle_filepath).read
     @solution = File.open(solution_filepath).read
 
+    @solution_diff = _get_solution_diff
     if @puzzle.length != @solution.length
-        raise BadInputDataError, "Puzzle and solution do not have the same number of characters and are therefore invalid"
+      raise BadInputDataError, "Puzzle and solution do not have the same number of characters and are therefore invalid"
     end
   end
 
-  def get_diff
-     
+  def _get_solution_diff
+    diff = {}
+    @puzzle.split("").each_with_index do |puzzle_letter, pos|
+      solution_letter = @solution[pos]
+      if puzzle_letter == "_" and solution_letter != "_" 
+        diff[solution_letter] ||= []
+        diff[solution_letter].push pos 
+      end
+    end
+    diff 
   end
 
+  def guess(symbol)
+    raise InvalidGuessError if not valid_guess?(symbol)
+    if @solution_diff.include?(symbol)
+      @solution_diff[symbol].count
+    else
+      0
+    end
+  end
+
+  def valid_guess?(symbol)
+    return false if ["_", " "].include?(symbol)
+    return false if symbol.length > 1 
+    true
+  end
 end
 
+class InvalidGuessError < StandardError; end
 class BadInputDataError < StandardError; end
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -9,15 +9,52 @@ describe Hangman do
     end
 
     it "should reject puzzle and solution if they do not have the same number of characters" do
-      lambda { @hangman.load("spec/sample_puzzle.txt", "spec/invalid_sample_solution.txt")}.should raise_error(BadInputDataError)
+      lambda { @hangman.load("spec/sample_puzzle.txt", "spec/invalid_sample_solution.txt") }.should raise_error(BadInputDataError)
     end    
 
-    it "should return nothing upon successfully loading valid puzzle and solution files" do
-      @hangman.load("spec/sample_puzzle.txt", "spec/sample_solution.txt").should do_nothing
+    it "should generate a hash that maps each character that is part of the solution to a position in the string" do
+      @hangman.load("spec/sample_puzzle.txt", "spec/sample_solution.txt")
+      correct_solution_diff = { 
+        "i"=>[60], 
+        "f"=>[61, 81], 
+        "a"=>[63, 82], 
+        "="=>[64,65],
+        "b"=>[66], 
+        "r"=>[74, 78], 
+        "e"=>[75, 85, 91],
+        "t"=>[76], 
+        "u"=>[77],
+        "n"=>[79, 92], 
+        "l"=>[83], 
+        "s"=>[84], 
+        "d"=>[93] 
+      }     
+          
+      @hangman.solution_diff.should == correct_solution_diff
+    end
+  end
+
+  describe "#guess" do
+    before(:each) do
+      @hangman = Hangman.new
+      @hangman.load("spec/sample_puzzle.txt", "spec/sample_solution.txt")
     end
 
-    it "should generate a hash that maps each character that is part of the solution to a position in the string" do
-      fail
+    it "should return the number of occurrences when the symbol is in the puzzle" do
+      @hangman.guess("a").should == 2
+      @hangman.guess("r").should == 2
+      @hangman.guess("i").should == 1
+    end
+
+    it "should return 0 when the symbol is valid but is not in the puzzle" do
+      @hangman.guess("z").should == 0
+    end 
+
+    it "should raise an InvalidGuessError if it is not an acceptable guess string" do
+      invalid_characters = %w(aa _) 
+      invalid_characters.each do |invalid_character|
+        lambda { @hangman.guess(invalid_character) }.should raise_error(InvalidGuessError) 
+      end
     end
   end
 end
